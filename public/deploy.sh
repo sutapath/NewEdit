4b9f2463be9433de2e70c6a5e6bf3114850ad76d
sutaphat <suta_phat@srru.ac.th>
2025-03-11 14:49:39 +0700

    editlast

--- public/deploy.sh
+++ public/deploy.sh
@@ -7,6 +7,6 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache 
-php artisan serve --host=0.0.0.0 --port=8000  
+
 # echo "Running migrations..."
 # php artisan migrate --force
