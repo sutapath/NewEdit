62ee992cb37eed28654439a7d999feaae9d91171
sutaphat <suta_phat@srru.ac.th>
2025-03-11 10:25:38 +0700

    deploy.sh

---
+++ public/deploy.sh
@@ -0,0 +1,13 @@
+
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+php artisan serve
+# echo "Running migrations..."
+# php artisan migrate --force
