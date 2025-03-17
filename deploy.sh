fc9f2bb1fd37ff5b6311bf5f4011e20f8722ceee
sutaphat <suta_phat@srru.ac.th>
2025-03-17 11:53:46 +0700

    testt

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-
-echo "Running composer"
-echo "Caching config..."
-C:\xampp\php\php artisan config:cache  
-C:\xampp\php\php artisan route:cache  
+  
+"C:/xampp/php/php" artisan config:cache  
+echo "Caching routes..."  
+"C:/xampp/php/php" artisan route:cache  
