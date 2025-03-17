67210bf4a09ef9eed70623205d444a5d8927bf43
sutaphat <suta_phat@srru.ac.th>
2025-03-17 11:50:56 +0700

    test

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 
 echo "Running composer"
-# composer install --no-dev --working-dir=/var/www/html
-
 echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache 
-
-# echo "Running migrations..."
-# php artisan migrate --force
+C:\xampp\php\php artisan config:cache  
+C:\xampp\php\php artisan route:cache  
